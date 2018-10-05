63007eccd3746db09ecf6836ca206c7be68dbafc
Ryan Ha <ryan@hugsbyha.com>
2018-10-05 12:15:34 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+
+const express = require('express')
+const hbs = require('hbs')
+
+
+var app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+    //response.send('<h1>hello express</h1>');
+    // response.send({
+    //     name: 'Ryan',
+    //     likes: [
+    //         'Biking',
+    //         'Eating'
+    //     ]
+    // })
+
+    response.render('home.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'test'
+    })
+})
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'Unable to do'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+})
